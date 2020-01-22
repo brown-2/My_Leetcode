e179f38ce4f222b84a74eec25f032a554f31dae2
brown-2 <aptx4609@163.com>
2020-01-22 12:16:03 +0800

    none

--- test.py
+++ test.py
@@ -1,20 +1,18 @@
+
 from typing import List
 class Solution:
-    def longestValidParentheses(self, s: str) -> int:
-        record = []
-        while '()' in s:
-            record.append(s.index('()'))
-            s = s.replace('()', '', 1)
-        return record
-
-
+    def addToArrayForm(self, A: List[int], K: int) -> List[int]:
+        return list(str(int(''.join([str(i) for i in A])) + K)) if A != [] else [1]
+                    
 
 
 def main():
     obj = Solution()
-    s = "(()())((())"
-    print(obj.longestValidParentheses(s))
+    A = [1,2,0,0]; K = 34
+    print(obj.addToArrayForm(A, K))
+    
 
 
 if __name__ == '__main__':
     main()
+
