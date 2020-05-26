fc31f6439ad33dbd6a321aee9a8b4c11f9dc4200
brown-2 <aptx4609@163.com>
2020-05-26 12:58:38 +0800

    new

--- test.py
+++ test.py
@@ -1,33 +1,42 @@
 from typing import List
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
-    def largestRectangleArea(self, heights: List[int]) -> int:
-        if not heights:
-            return 0
-        dp = [heights[0]]
-        res = dp[0]
-        for i in range(1, len(heights)):
-            j = i - 1
-            if dp[j] > heights[i]:
-                res = max(res, max((dp[-i] * i for i in range(1, len(dp) + 1))))
-            while j >= 0:
-                if dp[j] > heights[i]:
-                    dp[j] = heights[i]
-                    j -= 1
-                else:
-                    break
-            dp.append(heights[i])
-        res = max(res, max((dp[-i] * i for i in range(1, len(dp) + 1))))
-        return res
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        if not preorder:
+            return None
+        root = TreeNode(preorder[0])
+        index = inorder.index(root.val)
+        l_inorder = inorder[:index]
+        r_inorder = inorder[index + 1:]
 
+        l_preorder = preorder[1:1 + len(l_inorder)]
+        r_preorder = preorder[index + 1:index + 1 + len(r_inorder)]
+
+        root.left = self.buildTree(l_preorder, l_inorder)
+        root.right = self.buildTree(r_preorder, r_inorder)
+        return root
             
-
-
-
+def print_tree(root):
+    if root == None:
+        print('null', end = ' ')
+    else:
+        print(root.val, end = ' ')
+        print_tree(root.left)
+        print_tree(root.right)
 
 def main():
     obj = Solution()
-    heights = [2,1,5,6,2,3]
-    print(obj.largestRectangleArea(heights))
+    preorder = [3,9,20,15,7]
+    inorder = [9,3,15,20,7]
+    preorder = [1,2,3]
+    inorder = [3,2,1]
+    tree = obj.buildTree(preorder, inorder)
+    print_tree(tree)
 
 
 
